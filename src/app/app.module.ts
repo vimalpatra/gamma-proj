48749ac8ce75f45c7a9cbc7032b4b2df437ef226
vimal patra <vimal.patra@hostbooks.com>
2020-12-17 11:32:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const routes: Routes = [
+	{ path: '', pathMatch: 'full', component: ExperienceComponent },
+	{ path: '/signup', component: SignupComponent },
+	{ path: '/login', component: LoginComponent },
+  ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	AppRoutingModule,
+	RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
